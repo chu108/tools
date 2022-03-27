8c5608dbf7b52732777b5f7bf82dcdd1185bc266
chu108 <879656448@qq.com>
2022-03-27 16:05:27 +0800

    1

--- _arr/slice.go
+++ _arr/slice.go
@@ -1,121 +1,93 @@
 package _arr
 
 import (
-	"github.com/chu108/tools/_num"
-	"github.com/chu108/tools/_str"
+	"fmt"
+	"strconv"
 )
 
-func StrToInt64(req []string) (res []int64) {
-	length := len(req)
-	res = make([]int64, 0, length)
-	for i := 0; i < length; i++ {
-		temp := _str.ToInt64(req[i])
-		res = append(res, temp)
+type Int interface {
+	int | int32 | int64 | uint | uint32 | uint64
+}
+
+type Float interface {
+	float32 | float64
+}
+
+//ToMap 转Map
+func ToMap[T any](s []T) (res map[T]bool) {
+	res = make(map[T]bool, 0)
+	for i := 0; i < len(s); i++ {
+		res[s[i]] = true
 	}
 	return
 }
 
-func StrToMap(req []string) (res map[string]bool) {
-	length := len(req)
-	res = make(map[string]bool, length)
-	for i := 0; i < length; i++ {
-		res[req[i]] = true
-	}
-	return
-}
-
-func StrToSql(req []string) (query, args []string) {
-	length := len(req)
-	query = make([]string, 0, length)
-	args = make([]string, 0, length)
-	for i := 0; i < length; i++ {
-		query = append(query, "?")
-		args = append(args, req[i])
-	}
-	return
-}
-
-func Int64ToStr(req []int64) (res []string) {
-	length := len(req)
-	res = make([]string, 0, length)
-	for i := 0; i < length; i++ {
-		res = append(res, _num.Int64ToStr(req[i]))
-	}
-	return
-}
-
-func Int64ToMap(req []int64) (res map[int64]bool) {
-	length := len(req)
-	res = make(map[int64]bool, length)
-	for i := 0; i < length; i++ {
-		res[req[i]] = true
-	}
-	return
-}
-
-func Int64ToSql(req []int64) (query []string, args []int64) {
-	length := len(req)
-	query = make([]string, 0, length)
-	args = make([]int64, 0, length)
-	for i := 0; i < length; i++ {
-		query = append(query, "?")
-		args = append(args, req[i])
-	}
-	return
-}
-
-func IntToStr(req []int) (res []string) {
-	length := len(req)
-	res = make([]string, 0, length)
-	for i := 0; i < length; i++ {
-		res = append(res, _num.IntToStr(req[i]))
-	}
-	return
-}
-
-func IntToMap(req []int) (res map[int]bool) {
-	length := len(req)
-	res = make(map[int]bool, length)
-	for i := 0; i < length; i++ {
-		res[req[i]] = true
-	}
-	return
-}
-
-func IntToSql(req []int) (query []string, args []int) {
-	length := len(req)
-	query = make([]string, 0, length)
-	args = make([]int, 0, length)
-	for i := 0; i < length; i++ {
-		query = append(query, "?")
-		args = append(args, req[i])
-	}
-	return
-}
-
-func InStrArr(val string, arr []string) bool {
+//Distinct 数组去重
+func Distinct[T comparable](arr []T) (res []T) {
+	temp := make(map[T]struct{}, 0)
+	res = make([]T, 0, len(arr))
 	for i := 0; i < len(arr); i++ {
-		if val == arr[i] {
+		if _, ok := temp[arr[i]]; !ok {
+			temp[arr[i]] = struct{}{}
+			res = append(res, arr[i])
+		}
+	}
+	return
+}
+
+//Exists 判断某个是否存在
+func Exists[T comparable](arr []T, val T) bool {
+	l := len(arr)
+	if l == 0 {
+		return false
+	}
+	for i := 0; i < l; i++ {
+		if arr[i] == val {
 			return true
 		}
 	}
 	return false
 }
 
-func InInt64Arr(val int64, arr []int64) bool {
-	for i := 0; i < len(arr); i++ {
-		if val == arr[i] {
-			return true
+//In 判断多个值是否存在，返回已存在的值数组
+func In[T comparable](arr []T, vales ...T) (res []T) {
+	al, vl := len(arr), len(vales)
+	res = make([]T, 0, vl)
+	if al == 0 || vl == 0 {
+		return
+	}
+	valMap := make(map[T]struct{}, 0)
+	for i := 0; i < vl; i++ {
+		valMap[vales[i]] = struct{}{}
+	}
+	for i := 0; i < al; i++ {
+		if _, ok := valMap[arr[i]]; ok {
+			res = append(res, arr[i])
 		}
 	}
-	return false
+	return
 }
 
-func InIntArr(val int, arr []int) bool {
-	for i := 0; i < len(arr); i++ {
-		if val == arr[i] {
-			return true
+// StrToInt 字符串数组转int数组
+func StrToInt(str []string) (res []int, err error) {
+	l := len(str)
+	res = make([]int, 0, l)
+	for i := 0; i < l; i++ {
+		tmp, err := strconv.Atoi(str[i])
+		if err != nil {
+			return nil, err
 		}
+		res = append(res, tmp)
 	}
-	return false
+	return
+}
+
+// IntToStr int数组转字符串数组
+func IntToStr[T Int](str []T) (res []string, err error) {
+	l := len(str)
+	res = make([]string, 0, l)
+	for i := 0; i < l; i++ {
+		res = append(res, fmt.Sprintf("%d", str[i]))
+	}
+	return
 }
--- _map/map.go
+++ _map/map.go
@@ -1,153 +1,16 @@
 package _map
 
-func IntBoolToKeys(req map[int]bool) (res []int) {
-	length := len(req)
-	res = make([]int, 0, length)
-	for k := range req {
+func Keys[K comparable, V any](m map[K]V) (res []K) {
+	res = make([]K, 0, len(m))
+	for k := range m {
 		res = append(res, k)
 	}
 	return
 }
 
-func IntIntToKeys(req map[int]int) (res []int) {
-	length := len(req)
-	res = make([]int, 0, length)
-	for k := range req {
-		res = append(res, k)
-	}
-	return
-}
-
-func IntStrToKeys(req map[int]string) (res []int) {
-	length := len(req)
-	res = make([]int, 0, length)
-	for k := range req {
-		res = append(res, k)
-	}
-	return
-}
-
-func Int64BoolToKeys(req map[int64]bool) (res []int64) {
-	length := len(req)
-	res = make([]int64, 0, length)
-	for k := range req {
-		res = append(res, k)
-	}
-	return
-}
-
-func Int64Int64ToKeys(req map[int64]int64) (res []int64) {
-	length := len(req)
-	res = make([]int64, 0, length)
-	for k := range req {
-		res = append(res, k)
-	}
-	return
-}
-
-func Int64StrToKeys(req map[int64]string) (res []int64) {
-	length := len(req)
-	res = make([]int64, 0, length)
-	for k := range req {
-		res = append(res, k)
-	}
-	return
-}
-
-func StrBoolToKeys(req map[string]bool) (res []string) {
-	length := len(req)
-	res = make([]string, 0, length)
-	for k := range req {
-		res = append(res, k)
-	}
-	return
-}
-
-func StrStrToKeys(req map[string]string) (res []string) {
-	length := len(req)
-	res = make([]string, 0, length)
-	for k := range req {
-		res = append(res, k)
-	}
-	return
-}
-
-func StrInt64ToKeys(req map[string]int64) (res []string) {
-	length := len(req)
-	res = make([]string, 0, length)
-	for k := range req {
-		res = append(res, k)
-	}
-	return
-}
-
-func StrIntToKeys(req map[string]int) (res []string) {
-	length := len(req)
-	res = make([]string, 0, length)
-	for k := range req {
-		res = append(res, k)
-	}
-	return
-}
-
-func IntIntToValues(req map[int]int) (res []int) {
-	length := len(req)
-	res = make([]int, 0, length)
-	for _, v := range req {
-		res = append(res, v)
-	}
-	return
-}
-
-func IntStrToValues(req map[int]string) (res []string) {
-	length := len(req)
-	res = make([]string, 0, length)
-	for _, v := range req {
-		res = append(res, v)
-	}
-	return
-}
-
-func Int64Int64ToValues(req map[int64]int64) (res []int64) {
-	length := len(req)
-	res = make([]int64, 0, length)
-	for _, v := range req {
-		res = append(res, v)
-	}
-	return
-}
-
-func Int64StrToValues(req map[int64]string) (res []string) {
-	length := len(req)
-	res = make([]string, 0, length)
-	for _, v := range req {
-		res = append(res, v)
-	}
-	return
-}
-
-func StrStrToValues(req map[string]string) (res []string) {
-	length := len(req)
-	res = make([]string, 0, length)
-	for _, v := range req {
-		res = append(res, v)
-	}
-	return
-}
-
-func StrInt64ToValues(req map[string]int64) (res []int64) {
-	length := len(req)
-	res = make([]int64, 0, length)
-	for _, v := range req {
-		res = append(res, v)
-	}
-	return
-}
-
-func StrIntToValues(req map[string]int) (res []int) {
-	length := len(req)
-	res = make([]int, 0, length)
-	for _, v := range req {
+func Values[K comparable, V any](m map[K]V) (res []V) {
+	res = make([]V, 0, len(m))
+	for _, v := range m {
 		res = append(res, v)
 	}
 	return
